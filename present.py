88b0362ddc273e70a3ff5d3473e27e0830ddb372
Maciej Olko <maciej.olko@yougov.com>
2021-08-31 00:14:48 +0200

    Initial commit

---
+++ present.py
@@ -0,0 +1,31 @@
+import re
+from collections import Counter
+
+import polib
+
+import toml
+
+with open('languages/fr.toml') as rules_src:
+    rules = toml.load(rules_src)
+
+c = Counter()
+for package, domain in tuple(((package, 'django') for package in (
+    'contrib/admin', 'contrib/admindocs', 'contrib/auth', 'contrib/contenttypes', 'contrib/flatpages', 'contrib/gis',
+    'contrib/humanize', 'contrib/postgres', 'contrib/redirects',
+    'contrib/sessions', 'contrib/sites', 'conf'
+))) + (('contrib/admin', 'djangojs'),):
+    print(package)
+    pofile = polib.pofile(
+        f'/Users/maciek/projects/django/django/{package}/locale/fr/LC_MESSAGES/{domain}.po'
+    )
+
+    for entry in pofile:
+        if groups := re.findall(r'%(\(([a-z_]+)\))?s|{([a-z_]*)}', entry.msgid):
+            # print([match[1] or match[2] for match in groups])
+            param_names = [match[1] or match[2] for match in groups]
+            c.update(param_names)
+            # if "name" in param_names:
+            print(entry.msgid, entry.flags, entry.msgstr,
+                  rules.get(entry.msgid)
+                  )
+print(c)
