6b38ebc0444bdb488497a7d00027cb961818c330
Maciej Olko <maciej.olko@yougov.com>
2021-09-09 22:52:20 +0200

    Add plurals for parametrization

--- print_all_parametrized_messages.py
+++ print_all_parametrized_messages.py
@@ -16,34 +16,33 @@ with open(f'{language}.toml') as rules_src:
     rules = toml.load(rules_src)
 auth = polib.pofile(f'{django_clone_path}/django/contrib/auth/locale/{language}/LC_MESSAGES/django.po')
 user = auth.find('user').msgstr
+users = auth.find('users').msgstr
 group = auth.find('group').msgstr
+groups = auth.find('groups').msgstr
 permission = auth.find('permission').msgstr
-session = (
-    polib.pofile(f'{django_clone_path}/django/contrib/sessions/locale/{language}/LC_MESSAGES/django.po')
-    .find('session')
-    .msgstr
+permissions = auth.find('permissions').msgstr
+sessions_file = polib.pofile(f'{django_clone_path}/django/contrib/sessions/locale/{language}/LC_MESSAGES/django.po')
+session = sessions_file.find('session').msgstr
+sessions = sessions_file.find('sessions').msgstr
+sites_file = polib.pofile(f'{django_clone_path}/django/contrib/sites/locale/{language}/LC_MESSAGES/django.po')
+site = sites_file.find('site').msgstr
+sites = sites_file.find('sites').msgstr
+redirects_file = polib.pofile(f'{django_clone_path}/django/contrib/redirects/locale/{language}/LC_MESSAGES/django.po')
+redirect = redirects_file.find('redirect').msgstr
+redirects = redirects_file.find('redirects').msgstr
+flatpages_file = polib.pofile(f'{django_clone_path}/django/contrib/flatpages/locale/{language}/LC_MESSAGES/django.po')
+flatpage = flatpages_file.find('flat page').msgstr
+flatpages = flatpages_file.find('flat pages').msgstr
+admin = polib.pofile(f'{django_clone_path}/django/contrib/admin/locale/{language}/LC_MESSAGES/django.po')
+logentry = admin.find('log entry').msgstr
+logentries = admin.find('log entries').msgstr
+contenttypes_file = polib.pofile(
+    f'{django_clone_path}/django/contrib/contenttypes/locale/{language}/LC_MESSAGES/django.po'
 )
-site = (
-    polib.pofile(f'{django_clone_path}/django/contrib/sites/locale/{language}/LC_MESSAGES/django.po')
-    .find('site')
-    .msgstr
-)
-redirect = (
-    polib.pofile(f'{django_clone_path}/django/contrib/redirects/locale/{language}/LC_MESSAGES/django.po')
-    .find('redirect')
-    .msgstr
-)
-flatpage = (
-    polib.pofile(f'{django_clone_path}/django/contrib/flatpages/locale/{language}/LC_MESSAGES/django.po')
-    .find('flat page')
-    .msgstr
-)
-logentry = (
-    polib.pofile(f'{django_clone_path}/django/contrib/admin/locale/{language}/LC_MESSAGES/django.po')
-    .find('log entry')
-    .msgstr
-)
-contenttype = auth.find('content type').msgstr
+contenttype = contenttypes_file.find('content type').msgstr
+contenttypes = contenttypes_file.find('content types').msgstr
+singulars = (user, group, permission, session, site, redirect, flatpage, logentry, contenttype)
+plurals = (users, groups, permissions, sessions, sites, redirects, flatpages, logentries, contenttypes)
 c = Counter()
 for package, domain in (
     tuple(
@@ -75,11 +74,12 @@ for package, domain in (
             param_names = [match[1] or match[2] for match in groups]
             c.update(param_names)
             examples = []
-            for model in (user, group, permission, session, site, redirect, flatpage, logentry, contenttype):
-                try:
-                    examples.append(entry.msgstr % {param_names[0]: model})
-                except KeyError:
-                    pass
+            if param_names[0] == 'verbose_name_plural':
+                for model in plurals:
+                    try:
+                        examples.append(entry.msgstr % {param_names[0]: model})
+                    except KeyError:
+                        pass
             print(
                 entry.msgid,
                 entry.msgctxt,
