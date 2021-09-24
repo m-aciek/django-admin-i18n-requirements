a107fe5241218b25183fdf27445c1793e36ba1cf
Maciej Olko <maciej.olko@yougov.com>
2021-09-24 09:41:01 +0200

    Implement --print with-parameters flag for new script

--- print_improvements.py
+++ print_improvements.py
@@ -1,15 +1,22 @@
 from enum import Enum
 from pathlib import Path
+from re import findall
 
 from polib import pofile
 from toml import load
 from typer import echo, run
 
 
-class MessageSet(Enum):
+class MessageSet(str, Enum):
     all = 'all'
     improved = 'improved'
-    with_parameters = 'with_parameters'
+    with_parameters = 'with-parameters'
+
+
+def get_parameters(msgid: str) -> list[str]:
+    if match_groups := findall(r'%(\(([a-z_]+)\))?[ds]|{([a-z_]*)}', msgid):
+        return [match[1] or match[2] for match in match_groups]
+    return []
 
 
 def print_improvements(django_clone_path: Path, language: str, print: MessageSet = MessageSet.improved) -> None:
@@ -21,7 +28,7 @@ def print_improvements(django_clone_path: Path, language: str, print: MessageSet
     for entry in admin:
         if print == MessageSet.improved and entry.msgid not in improvements:
             continue
-        if print == MessageSet.with_parameters and None:  # and doesn't have parameters
+        if print == MessageSet.with_parameters and not get_parameters(entry.msgid):
             continue
         echo(entry.msgid)
 
