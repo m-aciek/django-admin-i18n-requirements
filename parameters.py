720bc83b6365fb3183b7bd482f1bcf730b79abbc
Maciej Olko <maciej.olko@yougov.com>
2021-09-25 10:54:32 +0200

    Fetch translations in new script

--- parameters.py
+++ parameters.py
@@ -1,2 +1,9 @@
 model_verbose_name = None
-parameters_mapping = {'Select %s to change': {'': model_verbose_name}}
+model_verbose_name_plural = None
+parameters_mapping = {
+    'Select %s': {'': model_verbose_name},
+    'Select %s to change': {'': model_verbose_name},
+    'Select %s to view': {'': model_verbose_name},
+    'Delete selected %(verbose_name_plural)s': {'verbose_name_plural': model_verbose_name_plural},
+    'Successfully deleted %(count)d %(items)s.': {},
+}
--- print_improvements.py
+++ print_improvements.py
@@ -3,8 +3,13 @@ from pathlib import Path
 from re import findall
 
 from polib import pofile, POEntry
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
 from toml import load
-from typer import echo, run
+from typer import run
+
+from parameters import parameters_mapping
 
 
 class MessageSet(str, Enum):
@@ -25,19 +30,43 @@ def order(entry: POEntry):
     return entry
 
 
+def format_translation_with_examples(entry: POEntry) -> Text:
+    current = Text()
+    current.append('\n'.join(f'{path}:{line}' for path, line in entry.occurrences) + '\n')
+    current.append(entry.msgid + '\n', "bold")
+    current.append(entry.msgstr + '\n', "bold")
+    return current
+
+
 def print_improvements(django_clone_path: Path, language: str, print: MessageSet = MessageSet.improved) -> None:
-    admin = pofile(
-        str(django_clone_path / 'django' / 'contrib' / 'admin' / 'locale' / 'en' / 'LC_MESSAGES' / 'django.po')
-    )
+    admin_keys = admin_pofile(django_clone_path, 'en')
+    admin = admin_pofile(django_clone_path, language)
     with open(f'{language}.toml') as rules_src:
         improvements = load(rules_src)
-    for entry in sorted(admin, key=order):
+    rendered_improvements = []
+    for entry in sorted(admin_keys, key=order)[:4]:
+        translated_entry = admin.find(entry.msgid)
         if print == MessageSet.improved and entry.msgid not in improvements:
             continue
         parameters = get_parameters(entry.msgid)
         if print == MessageSet.with_parameters and not parameters:
             continue
-        echo([entry.occurrences, entry.msgid])
+        enhanced_parameters = [parameters_mapping[entry.msgid][param] for param in parameters]
+        rendered_improvements.append(entry)
+
+    table = Table(show_lines=True)
+    table.add_column('current translation with examples')
+    table.add_column('enhanced translation')
+    table.add_column('enhanced examples')
+    for improvement in rendered_improvements:
+        table.add_row(format_translation_with_examples(improvement))
+    Console().print(table)
+
+
+def admin_pofile(django_clone_path: Path, language: str):
+    return pofile(
+        str(django_clone_path / 'django' / 'contrib' / 'admin' / 'locale' / language / 'LC_MESSAGES' / 'django.po')
+    )
 
 
 if __name__ == '__main__':
