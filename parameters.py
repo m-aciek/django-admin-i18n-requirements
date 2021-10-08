9dc9346f51800898b2455449613f3399e13286e1
Maciej Olko <maciej.olko@yougov.com>
2021-10-08 21:30:07 +0200

    Simplify parameters module internals

--- parameters.py
+++ parameters.py
@@ -1,15 +1,6 @@
 from dataclasses import dataclass
-from typing import Union
-
-from polib import POEntry
 
 from django_resources import DjangoResources
-
-
-@dataclass(frozen=True)
-class ParametersSet:
-    name: str
-    parameters: list[Union[int, POEntry]]
 
 
 @dataclass(frozen=True)
@@ -18,27 +9,27 @@ class DjangoMessagesParameters:
 
     def model_verbose_name(self):
         auth = self.django_resources.django_pofile('contrib/auth')
-        return ParametersSet("model_verbose_name", [auth.find('user'), auth.find('group')])
+        return [auth.find('user'), auth.find('group')]
 
     def model_verbose_name_plural(self):
         auth = self.django_resources.django_pofile('contrib/auth')
-        return ParametersSet("model_verbose_name_plural", [auth.find('users'), auth.find('groups')])
+        return [auth.find('users'), auth.find('groups')]
 
     def model_ngettext(self):
         return (
-            [(1, model) for model in self.model_verbose_name().parameters]
-            + [(2, models) for models in self.model_verbose_name_plural().parameters]
-            + [(5, models) for models in self.model_verbose_name_plural().parameters]
+            [(1, model) for model in self.model_verbose_name()]
+            + [(2, models) for models in self.model_verbose_name_plural()]
+            + [(5, models) for models in self.model_verbose_name_plural()]
         )
 
     def parameters_mapping(self):
         return {
-            'Select %s': [{'': value} for value in self.model_verbose_name().parameters],
-            'Select %s to change': [{'': value} for value in self.model_verbose_name().parameters],
-            'Select %s to view': [{'': value} for value in self.model_verbose_name().parameters],
-            'Add %(name)s': [{'name': value} for value in self.model_verbose_name().parameters],
+            'Select %s': [{'': value} for value in self.model_verbose_name()],
+            'Select %s to change': [{'': value} for value in self.model_verbose_name()],
+            'Select %s to view': [{'': value} for value in self.model_verbose_name()],
+            'Add %(name)s': [{'name': value} for value in self.model_verbose_name()],
             'Delete selected %(verbose_name_plural)s': [
-                {'verbose_name_plural': value} for value in self.model_verbose_name_plural().parameters
+                {'verbose_name_plural': value} for value in self.model_verbose_name_plural()
             ],
             'Successfully deleted %(count)d %(items)s.': [
                 {'count': count, 'items': items} for count, items in self.model_ngettext()
