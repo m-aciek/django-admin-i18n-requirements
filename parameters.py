a018e20867963a4b2b0a437c7b744f7d7dc35cf5
Maciej Olko <maciej.olko@yougov.com>
2021-10-08 20:54:21 +0200

    Refactor parameters module to provide list of parameter dicts instead of dict of lists

--- parameters.py
+++ parameters.py
@@ -24,18 +24,23 @@ class DjangoMessagesParameters:
         auth = self.django_resources.django_pofile('contrib/auth')
         return ParametersSet("model_verbose_name_plural", [auth.find('users'), auth.find('groups')])
 
-    def number(self):
-        return ParametersSet("number", [1, 2, 5])
+    def model_ngettext(self):
+        return (
+            [(1, model) for model in self.model_verbose_name().parameters]
+            + [(2, models) for models in self.model_verbose_name_plural().parameters]
+            + [(5, models) for models in self.model_verbose_name_plural().parameters]
+        )
 
     def parameters_mapping(self):
         return {
-            'Select %s': {'': self.model_verbose_name()},
-            'Select %s to change': {'': self.model_verbose_name()},
-            'Select %s to view': {'': self.model_verbose_name()},
-            'Add %(name)s': {'name': self.model_verbose_name()},
-            'Delete selected %(verbose_name_plural)s': {'verbose_name_plural': self.model_verbose_name_plural()},
-            'Successfully deleted %(count)d %(items)s.': {
-                'count': self.number(),
-                'items': self.model_verbose_name_plural(),
-            },
+            'Select %s': [{'': value} for value in self.model_verbose_name().parameters],
+            'Select %s to change': [{'': value} for value in self.model_verbose_name().parameters],
+            'Select %s to view': [{'': value} for value in self.model_verbose_name().parameters],
+            'Add %(name)s': [{'name': value} for value in self.model_verbose_name().parameters],
+            'Delete selected %(verbose_name_plural)s': [
+                {'verbose_name_plural': value} for value in self.model_verbose_name_plural().parameters
+            ],
+            'Successfully deleted %(count)d %(items)s.': [
+                {'count': count, 'items': items} for count, items in self.model_ngettext()
+            ],
         }
